37a85adacef59ece595d755a00e76ef292345b22
Daniel Martin <etd@nomejortu.com>
2012-07-14 10:46:48 +0200

    Include ParseJson middleware in Faraday stack
    This will convert a JSON strings HTTP body into a Ruby Hash.
    
    All responses from the server are in JSON format.

---
+++ spec/faraday_spec.rb
@@ -0,0 +1,25 @@
+describe 'Faraday response' do
+
+  context "for valid object" do
+    let(:middleware) { VulnDBHQ::Response::ParseJson.new(lambda{|env| Faraday::Response.new(env)}) }
+
+    def process(body, content_type = nil)
+      env = {:body => body, :request_headers => Faraday::Utils::Headers.new}
+      env[:request_headers]['content-type'] = content_type if content_type
+      middleware.call(env)
+    end    
+
+    it "parses response body (JSON) into a Hash" do
+      response = process('{"a":1,"b":"dos"}', 'application/json')
+      response.body.should be_a_kind_of(Hash)
+      response.body.keys.should include(:a)
+      response.body.keys.should include(:b)
+      response.body[:b].should eq('dos')
+    end
+  end
+
+  context "for invalid object" do
+    let(:client) { VulnDBHQ::Client.new }
+    pending "handles 404 responses"
+  end
+end
